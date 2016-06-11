2f42c7f3f786757c881ab6488469699dbd447377
Stephen Lane-Walsh <sdl.slane@gmail.com>
2016-06-11 15:51:26 -0400

    Split things into multiple files

---
+++ src/shaders.h
@@ -0,0 +1,32 @@
+#ifndef SHADERS_H
+#define SHADERS_H
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "debug.h"
+#include "util.h"
+
+#define MAX_SHADER_COUNT     20
+#define MAX_SHADER_SIZE      1000000
+#define MAX_SHADER_LOG_SIZE  100000
+#define MAX_PROGRAM_LOG_SIZE MAX_SHADER_LOG_SIZE
+
+typedef struct ShaderInfo
+{
+    const char * filename;
+    GLenum type;
+
+} ShaderInfo;
+
+bool PrintShaderProgramLog(GLuint program);
+bool PrintShaderLog(GLuint shader);
+
+GLuint LoadShader(const char * filename, GLenum shader_type);
+GLuint LoadShaderProgram(ShaderInfo * shaders);
+
+#endif // SHADERS_H
---
+++ src/util.h
@@ -0,0 +1,10 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stdio.h>
+
+#include "debug.h"
+
+long int GetFileSize(FILE * fp);
+
+#endif // UTIL_H
