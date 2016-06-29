dde3e56eed839ab48399192cdd664bf4142e9ade
Stephen Lane-Walsh <slanewalsh@mywebgrocer.com>
2016-06-29 16:21:55 -0400

    Worked on OBJ loader, sourcing from https://github.com/syoyo/tinyobjloader

--- src/obj.c
+++ src/obj.c
@@ -4,28 +4,125 @@
 #include <stdlib.h>
 #include "debug.h"
 
+Material*
+Material_Create()
+{
+    Material* matl = (Material*)malloc(sizeof(Material));
+    CHECK_MEM(matl);
+
+    matl->name = NULL;
+    matl->ambient = { { 0.0f, 0.0f, 0.0f } };
+    matl->diffuse = { { 0.0f, 0.0f, 0.0f } };
+    matl->specular = { { 0.0f, 0.0f, 0.0f } };
+    matl->transmittance = { { 0.0f, 0.0f, 0.0f } };
+    matl->emission = { { 0.0f, 0.0f, 0.0f } };
+    matl->shininess = 1.0f;
+    matl->ior = 1.0f;
+    matl->dissolve = 1.0f;
+    matl->illum = 0;
+    matl->ambient_texname = NULL;
+    matl->diffuse_texname = NULL;
+    matl->specular_texname = NULL;
+    matl->specular_highlight_texname = NULL;
+    matl->bump_texname = NULL;
+    matl->displacement_texname = NULL;
+    matl->alpha_texname = NULL;
+
+    return matl;
+
+error:
+    Material_Destroy(matl);
+
+    return NULL;
+}
+
+void
+Material_Destroy(Material* matl)
+{
+    if (!matl)
+        return;
+
+    free(matl->name);
+    free(matl->ambient_texname);
+    free(matl->diffuse_texname);
+    free(matl->specular_texname);
+    free(matl->specular_highlight_texname);
+    free(matl->bump_texname);
+    free(matl->displacement_texname);
+    free(matl->alpha_texname);
+    free(matl);
+}
+
+Mesh*
+Mesh_Create()
+{
+    Mesh* mesh = (Mesh*)malloc(sizeof(Mesh));
+    CHECK_MEM(mesh);
+
+    mesh->positions = NULL;
+    mesh->normals = NULL;
+    mesh->texcoords = NULL;
+    mesh->indices = NULL;
+
+    return mesh;
+
+error:
+    Mesh_Destroy(mesh);
+
+    return NULL;
+}
+
+void
+Mesh_Destroy(Mesh* mesh)
+{
+    if (!mesh)
+        return;
+
+    free(mesh->positions);
+    free(mesh->normals);
+    free(mesh->texcoords);
+    free(mesh->indices);
+    free(mesh);
+}
+
+Shape*
+Shape_Create()
+{
+    Shape* shape = (Shape*)malloc(sizeof(Shape));
+    CHECK_MEM(shape);
+
+    shape->name = NULL;
+    shape->mesh = Mesh_Create();
+
+    return shape;
+
+error:
+    Shape_Destroy(shape);
+
+    return NULL;
+}
+
+void
+Shape_Destroy(Shape* shape)
+{
+    if (!shape)
+        return;
+
+    free(shape->name);
+    Mesh_Destroy(shape->mesh);
+    free(shape);
+}
+
 OBJ*
 OBJ_Create(int vertCount)
 {
     OBJ* obj = (OBJ*)malloc(sizeof(OBJ));
     CHECK_MEM(obj);
 
-    obj->vertices = (Vec3*)malloc(sizeof(Vec3) * vertCount);
-    CHECK_MEM(obj->vertices);
-
-    obj->normals = (Vec3*)malloc(sizeof(Vec3) * vertCount);
-    CHECK_MEM(obj->vertices);
-
-    obj->texCoords = (Vec2*)malloc(sizeof(Vec2) * vertCount);
-    CHECK_MEM(obj->vertices);
-
     return obj;
 
 error:
-    free(obj);
-    free(obj->vertices);
-    free(obj->normals);
-    free(obj->texCoords);
+    OBJ_Destroy(obj);
 
     return NULL;
 }
@@ -36,9 +133,6 @@ OBJ_Destroy(OBJ* obj)
     if (!obj)
         return;
 
-    free(obj->vertices);
-    free(obj->normals);
-    free(obj->texCoords);
     free(obj);
 }
 
--- src/obj.h
+++ src/obj.h
@@ -3,15 +3,62 @@
 
 #include "geom/vec.h"
 
+typedef struct Material
+{
+    char* name;
+
+    Vec3 ambient;
+    Vec3 diffuse;
+    Vec3 specular;
+    Vec3 transmittance;
+    Vec3 emission;
+    float shininess;
+    float ior;      // index of reflection
+    float dissolve; // 1 = opaque, 0 = fully transparent
+    // illumination model (see http://www.fileformat.info/format/material/)
+    int illum;
+
+    char* ambient_texname;            // map_Ka
+    char* diffuse_texname;            // map_Kd
+    char* specular_texname;           // map_Ks
+    char* specular_highlight_texname; // map_Ns
+    char* bump_texname;               // map_bump, bump
+    char* displacement_texname;       // disp
+    char* alpha_texname;              // map_d
+
+} Material;
+
+Material* Material_Create();
+void Material_Destroy(Material* matl);
+
+typedef struct Mesh
+{
+    Vec3* positions;
+    Vec3* normals;
+    Vec2* texcoords;
+    unsigned int* indices;
+
+} Mesh;
+
+Mesh* Mesh_Create();
+void Mesh_Destroy(Mesh* mesh);
+
+typedef struct Shape
+{
+    char* name;
+    Mesh* mesh;
+
+} Shape;
+
+Shape* Shape_Create();
+void Shape_Destroy(Shape* shape);
+
 typedef struct OBJ
 {
-    Vec3* vertices;
-    Vec3* normals;
-    Vec2* texCoords;
-} OBJ;
+    Shape** shapes;
+    Material** materials;
 
-OBJ* OBJ_Create(int vertCount);
-void OBJ_Destroy(OBJ* obj);
+} OBJ;
 
 OBJ* OBJ_Load(const char* filename);
 
